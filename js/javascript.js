113e885a1331a473d7da287fd77b28a4031d54e2
unknown <qlaudigital@gmail.com>
2021-04-13 21:10:00 +0800

    Mastering Repostory

---
+++ js/javascript.js
@@ -0,0 +1,3 @@
+document.write('<script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>');
+document.write('<script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>');
+document.write('<script type="text/javascript" src="js/lazyload.min.js"></script>');
